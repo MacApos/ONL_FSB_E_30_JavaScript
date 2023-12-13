9e820f2384347c4933086175b0fa35c3db4a8990
MacApos <maciejapostol98@gmail.com>
2023-12-13 22:10:36 +0100

    Dzień-4

--- 03_Dzien_4/02_Fetch/02_Zadanie_2/js/app.js
+++ 03_Dzien_4/02_Fetch/02_Zadanie_2/js/app.js
@@ -1,9 +1,33 @@
-fetch("https://pokeapi.co/api/v2/pokemon")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error();
-        }
-        return response
-    })
-    .then(pokemon => console.log(pokemon.items[0]))
-    .catch(err => console.warn(err));
+function getPokemons() {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error();
+            }
+            return response.json();
+        })
+        .then(pokemons => pokemons.results.forEach(pokemon => createPokemon(pokemon.name)))
+        .catch(err => console.warn(err));
+}
+
+function createPokemon(pokemon) {
+    const ul = document.querySelector("ul.list-group");
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.innerText = pokemon;
+    ul.appendChild(li);
+}
+
+Element.prototype.toggleClass = function (className) {
+    if(this.classList.contains(className)){
+        this.classList.remove(className);
+    } else {
+        this.classList.add(className);
+    }
+    console.log(this.classList)
+}
+
+const $body = document.querySelector("body");
+$body.toggleClass("kaka");
+
+getPokemons();
