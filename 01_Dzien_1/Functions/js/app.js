d79d37080350821e87996b4240e8836a057b01e3
MacApos <maciejapostol98@gmail.com>
2023-12-15 13:50:30 +0100

    Dzień-4

---
+++ 01_Dzien_1/Functions/js/app.js
@@ -0,0 +1,11 @@
+function StandardFunction(parameter) {
+    console.log(parameter);
+}
+
+const AnonymousFunction = function (parameter) {
+    console.log(parameter);
+}
+
+const ArrowFunction = (parameter) => {
+    console.log(parameter);
+}
